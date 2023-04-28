5b5c0bfb3ae5729ba8f5b9c6ad2d3af95d5154e1
Zhuge <785916801@qq.com>
2023-04-28 19:52:09 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,99 @@
+// pages/index/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+        let _this = this;
+        wx.request({
+            url:'https://buff.163.com/api/market/goods',
+            data:{
+                game:"csgo",
+                page_size:"20"
+            },
+            header:{
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            method:'GET',
+            success(res){
+                console.log(res.data)
+                _this.setData({
+                    items:res.data.data.items
+                })
+            }
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+        const sHash = currentTarget.dataset.items.market_hash_name
+            wx.request({
+                url: 'https://steamcommunity.com/market/priceoverview/',
+                data: {
+                  appid: 730,
+                  market_hash_name:sHash,
+                  currency: 23
+                },
+                header: {
+                  'content-type': 'application/json'
+                },
+                success(res) {
+                  console.log(res.data)
+                },
+                fail(err) {
+                  console.log(err)
+                }
+              })
+    },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /*
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
